4441b1c35817038da7d29208849c7d95bdf5b58c
Box Supremacy <j@box.lol>
2022-02-08 19:30:26 -0500

    Initial commit

---
+++ js/navbar.js
@@ -0,0 +1,39 @@
+// Insert navbar to header tags
+$(document).ready(function () {
+    $('header').load('/res/header.html');
+    $('footer').load('/res/footer.html');
+});
+
+// Add and remove responsive class from navbar
+// Used whenever opening or closing mobile navbar view
+function swapTopbarType() {
+    var x = document.getElementById('header');
+    if (x.classList.contains('topnav-content') && !x.classList.contains('responsive')) {
+        x.classList.add('responsive');
+    } else {
+        x.classList.remove('responsive');
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+    if ($(e.target).hasClass('dd-active')) return;
+
+    $('.show').map(function () {
+        this.classList.remove('show');
+    });
+
+    $('.dd-active').map(function () {
+        this.classList.remove('dd-active');
+    });
+};
+
+// Toggle selected dropdown
+function toggleDropdown(element, button) {
+    setTimeout(function () {
+        var dropdown = document.getElementById(element);
+        var buttonObject = document.getElementById(button);
+        dropdown.classList.toggle('show');
+        buttonObject.classList.toggle('dd-active');
+    }, 0.01);
+}
